0182733c3141d99682299c05c5cdca208a77c455
ibrahimorhan <ibrahim.orhan@boun.edu.tr>
2020-05-26 14:43:47 +0300

    updated

--- practice-app/cmpe352API/test_news.py
+++ practice-app/cmpe352API/test_news.py
@@ -5,7 +5,12 @@ class SimpleTest(unittest.TestCase):
         date = "2019-03-17"
         response = api_calls.get_nasa_news(date)
         self.assertEqual(response[0],"Please give a valid date.","Early date control failed")
-
+    #news
 
 if(__name__ == "__main__"):
     unittest.main()
+
+# 1 ay 1 gün öncesi fırat
+# ileri tarih fırat
+# input formatı date değil ibrahim
+# output format ibrahim
